5769ce261e07a8985ee21360d644e4aaad38fee7
Yeogwan Yoon <yoonsysm@gmail.com>
2026-09-25 15:52:35 +0900

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import "./css/App.css";
+import Favorites from "./pages/Favorites";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,12 @@
+import "../css/Favorites.css"
+
+function Favorites() {
+  return (
+    <div className="favorites-empty">
+      <h2>NO Favorite Movies Yet</h2>
+      <p>Start adding movies to your favorites and they will appear here!</p>
+    </div>
+  );
+}
+
+export default Favorites;
