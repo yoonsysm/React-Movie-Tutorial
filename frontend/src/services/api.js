0e5d85552f0b33b9afcc6167fccff7cee6866141
Yeogwan Yoon <yoonsysm@gmail.com>
2026-09-25 21:49:24 +0900

    api commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -8,7 +8,6 @@ export const getPopularMovies = async () => {
   return data.results;
 };
 
-
 export const searchMovies = async (query) => {
   const response = await fetch(
     `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
